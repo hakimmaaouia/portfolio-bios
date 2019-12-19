3515fd2a3b32aeb83f428573f4c8b7d0e5787d39
hakimmaaouia <hakimmaaouia@gmail.com>
2019-12-20 00:24:11 +0100

    nav1

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from "./styles/Nav.module.scss"
+
+const Nav = () => {
+    return(
+        <div className={style.navcontainer}>
+     
+      <ul className={style.stul}>
+          <li className={style.stli}>Hakim Design x86</li>
+          <li className={style.stli} >etytey</li>
+          <li className={style.stli} >tyey</li>
+          <li className={style.stli} >teyet
+          </li>
+          <li className={style.stli} >teyet</li>
+      </ul>
+
+            </div>
+    )
+}
+export default Nav ;
